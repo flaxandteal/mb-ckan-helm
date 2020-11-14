55f9932211949c98e0f8d5fdb6a99eee2c850eb0
Phil Weir <phil.weir@flaxandteal.co.uk>
2020-11-14 12:45:11 +0000

    pull in mb code and update helm charts for version

--- example/build.sh
+++ example/build.sh
@@ -2,6 +2,18 @@
 
 source $CKAN_VENV/bin/activate && cd $CKAN_VENV/src/
 
+ENVVARS_GIT_URL=https://github.com/okfn/ckanext-envvars
+ENVVARS_GIT_BRANCH=0.0.1
+S3FILESTORE_GIT_URL=https://github.com/keitaroinc/ckanext-s3filestore
+S3FILESTORE_GIT_BRANCH=efd5711
+
+pip install wheel
+pip install git+${ENVVARS_GIT_URL}@${ENVVARS_GIT_BRANCH}#egg=ckanext-envvars
+pip install git+${S3FILESTORE_GIT_URL}@${S3FILESTORE_GIT_BRANCH}#egg=ckanext-s3filestore
+pip install -r https://raw.githubusercontent.com/keitaroinc/ckanext-s3filestore/${S3FILESTORE_GIT_BRANCH}/requirements.txt
+curl -o /tmp/s3filestore.txt https://raw.githubusercontent.com/keitaroinc/ckanext-s3filestore/${S3FILESTORE_GIT_BRANCH}/requirements.txt
+pip install -r /tmp/s3filestore.txt
+
 git clone https://github.com/lintol/ckanext-oauth2provider
 cd ckanext-oauth2provider
 pip install -r dev-requirements.txt
